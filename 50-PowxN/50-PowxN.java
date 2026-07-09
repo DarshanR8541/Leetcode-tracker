6816fb29ee3819a138831bc0e82920a02aebdee3
Darshan R <darshanr8541@gmail.com>
2026-07-09 09:20:44 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:20:43 AM
+class Solution {
+    public double myPow(double x, int n) {
+        for(int i=0;i<n;i++){
+            return Math.pow(x,n);
+        }
+        return Math.pow(x,n);
+    }
+}
